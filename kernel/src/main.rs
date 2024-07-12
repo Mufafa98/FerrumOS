dc03f244728dcba97f1ef3b97101ad277b29acd8
Mufafa98 <marian.parlea2@gmail.com>
2024-07-12 15:45:23 +0300

    [Test] : Added VGA tests and refactorin

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,25 +1,16 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner_function)]
-#![reexport_test_harness_main = "test_main"]
-
-mod tests;
-
 use drivers::*;
 use io::serial_println;
 #[allow(unused_imports)]
 use utils::panic_module::panic;
-#[cfg(test)]
-use utils::test_runner_function;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     serial_println!("-----Kernel Entry Point-----");
     println!("Hello World{}", "!");
 
-    #[cfg(test)]
-    test_main();
+    // println!("tbGARvUwPLU2XRk0ebRJMteL3wfBt1kFx6jXynk5bhF59hmjtuf9Qx4DkgaHbPyGAmztbkP3WtYxRDuCZ");
 
     loop {}
 }
