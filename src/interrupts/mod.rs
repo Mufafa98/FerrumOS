5bf60d2869cd0f6a5715b3755b282da9a04899ff
Mufafa98 <46997399+Mufafa98@users.noreply.github.com>
2024-08-02 15:27:55 +0300

    Steeltooth (#2)
    * [Refactoring] : Migrated from workspace to crate
    
    * [Refactoring] : Migrated from workspace to crate
    
    * [Interrupt] : Double Faults, Keyboard & fixes
    
    * [Alocator] : Implemented basic paging
    
    * [Alocators] : Implemented Dummy allocator
    
    * [Alocators] : Added Bump and LinkedList allocators
    
    * [Alocators] : Implemented FixedSizeBlock allocator
    
    * [Concurrency] : Async/Await support & keyboard mods

---
+++ src/interrupts/mod.rs
@@ -0,0 +1,54 @@
+use lazy_static::lazy_static;
+use x86_64::structures::idt::InterruptDescriptorTable;
+//hardware
+use pic8259::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+    Keyboard,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+// end hardware
+mod handlers;
+mod tests;
+use handlers::*;
+
+use crate::gdt;
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
+        //hardware
+        idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        //end hardware
+        idt
+    };
+}
+pub fn init_idt() {
+    IDT.load();
+}
